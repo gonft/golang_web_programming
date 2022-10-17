6e99adf4ece7ab7f644ae8325195371e12c4a5d5
gonft <gonft.paul@gmail.com>
2022-10-17 23:35:46 +0900

    test: :white_check_mark: 멤버십 타입이 비어있는경우 MembershipTypeEmptyError가 발생하도록 적용

--- internal/repository.go
+++ internal/repository.go
@@ -7,6 +7,7 @@ import (
 
 var UserNameAlreadyExistsError = errors.New("이미 등록된 사용자 이름이 존재합니다.")
 var UserNameEmptyError = errors.New("사용자 이름이 비어있습니다.")
+var MembershipTypeEmptyError = errors.New("멤버십 타입이 비어있습니다.")
 
 type Repository struct {
 	data map[string]Membership
@@ -23,6 +24,8 @@ func (r *Repository) Save(request CreateRequest) (*CreateResponse, error) {
 		return nil, UserNameAlreadyExistsError
 	case request.UserName == "":
 		return nil, UserNameEmptyError
+	case request.MembershipType == "":
+		return nil, MembershipTypeEmptyError
 	}
 	r.data[request.UserName] = Membership{UserName: request.UserName, MembershipType: request.MembershipType}
 	return &CreateResponse{strconv.Itoa(len(r.data)), request.MembershipType}, nil
