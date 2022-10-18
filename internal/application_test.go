999f5557e1d929cfc5a4ec36af0eb6dd99b8f886
gonft <gonft.paul@gmail.com>
2022-10-19 00:44:51 +0900

    test: :white_check_mark: 사용자 이름를 입력하지 않고 갱신처리하는 경우 실패하는지 확인 하는 테스트 추가

--- internal/application_test.go
+++ internal/application_test.go
@@ -107,7 +107,7 @@ func TestUpdate(t *testing.T) {
 	})
 
 	t.Run("멤버십 아이디를 입력하지 않은 경우, 예외 처리한다.", func(t *testing.T) {
-		// given: 어플리케이션 멤버쉽 레포가 주어지고
+		// given: 어플리케이션 멤버쉽 레포에 사용자 jenny가 존재한다.
 		app := NewApplication(*NewRepository(map[string]Membership{}))
 		_, _ = app.Create(CreateRequest{"jenny", "naver"})
 
@@ -121,7 +121,17 @@ func TestUpdate(t *testing.T) {
 	})
 
 	t.Run("사용자 이름을 입력하지 않은 경우, 예외 처리한다.", func(t *testing.T) {
+		// given: 어플리케이션 멤버쉽 레포에 사용자 jenny가 존재한다.
+		app := NewApplication(*NewRepository(map[string]Membership{}))
+		_, _ = app.Create(CreateRequest{"jenny", "naver"})
 
+		// when: 사용자 이름을 입력하지 않고 갱신한다.
+		req := UpdateRequest{"1", "", "naver"}
+		res, err := app.Update(req)
+
+		// then: 실패한다.
+		assert.Nil(t, res)
+		assert.NotNil(t, err)
 	})
 
 	t.Run("멤버쉽 타입을 입력하지 않은 경우, 예외 처리한다.", func(t *testing.T) {
