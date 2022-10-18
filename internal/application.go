d24bfbfbb212936561069425dff642babebc8969
gonft <gonft.paul@gmail.com>
2022-10-19 00:35:25 +0900

    refactor: :recycle: 멤버쉽 생성 및 갱신 에러 처리 리팩토링

--- internal/application.go
+++ internal/application.go
@@ -8,13 +8,20 @@ func NewApplication(repository Repository) *Application {
 	return &Application{repository: repository}
 }
 
-func (app *Application) Create(request CreateRequest) (CreateResponse, error) {
+func (app *Application) Create(request CreateRequest) (*CreateResponse, error) {
 	membership, err := app.repository.Create(request)
-	return CreateResponse{membership.UserName, membership.MembershipType}, err
+	if err != nil {
+		return nil, err
+	}
+	return &CreateResponse{membership.UserName, membership.MembershipType}, nil
 }
 
-func (app *Application) Update(request UpdateRequest) (UpdateResponse, error) {
-	return UpdateResponse{"1", "jenny", "naver"}, nil
+func (app *Application) Update(request UpdateRequest) (*UpdateResponse, error) {
+	membership, err := app.repository.Update(request)
+	if err != nil {
+		return nil, err
+	}
+	return &UpdateResponse{membership.ID, membership.UserName, membership.MembershipType}, nil
 }
 
 func (app *Application) Delete(id string) error {
